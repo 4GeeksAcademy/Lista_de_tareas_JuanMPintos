c43bdf123b52907aeecbefcdffed2713df23ce08
Juan M. Pintos <166875657+JuanMPintos@users.noreply.github.com>
2024-08-07 15:22:14 +0000

    maquetado

--- src/js/component/Tareas.jsx
+++ src/js/component/Tareas.jsx
@@ -2,13 +2,8 @@ import React from "react";
 
 const Tareas = () => {
     return (
-        <div className="tarjeta">
-            <ul>
-                <li>
-                    <input />
-                </li>
-            </ul>
-        </div>)
+        <div>.</div>
+    )
 }
 
 
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Tareas from "./Tareas";
 
 const Home = () => {
+	const [inputValue, setInputValue] = useState("")
+	const [toDoList, setToDolist] = useState([])
+
 	return (
 		<div className="contenedor">
-		<h1>TODOS</h1>
-		<Tareas/>
+			<h1>To Do List</h1>
+			<ul>
+				<li><input type="text" onChange={(e) => setInputValue(e.target.value)} value={inputValue} placeholder="Que tarea vas a realizar?"></input></li>
+				<li>Hacer la cama <i class="fa-solid fa-trash-can"></i></li>
+				<li>Pasear el perro <i class="fa-solid fa-trash-can"></i></li>
+				<li>Ir al supermercado <i class="fa-solid fa-trash-can"></i></li>
+				<li>Mentoria con Javi <i class="fa-solid fa-trash-can"></i></li>
+				<li>Preparar la cena <i class="fa-solid fa-trash-can"></i></li>
+			</ul>
+			<div id="footer">10 tareas</div>
 		</div>
 	);
 };
